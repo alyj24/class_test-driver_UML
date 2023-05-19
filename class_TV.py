e0867515cc20b4cc2fffe32ed616ab9c148e71cb
Family <albon.136546120855@2depedqc.ph>
2023-05-19 18:01:28 +0800

    Pseudocode In

--- class_TV.py
+++ class_TV.py
@@ -6,3 +6,9 @@ print("\033[95m=" * 42)
 # create a python code that have a class named "TV" and a Test Driver program named
 # "TestTV" that will create two objects from class 'TV' and will produce the given outputs.
 
+# pseudocode
+# create a class and named as it is intructed
+# define attributes and set default methods
+# implement constructor
+# establish the methods for tv
+# end
