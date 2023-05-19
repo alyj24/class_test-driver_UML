b57f034a1e6b59824131d882ba76ab43c01e5796
Family <albon.136546120855@2depedqc.ph>
2023-05-19 18:26:22 +0800

    Methods for the TV is established

--- class_TV.py
+++ class_TV.py
@@ -22,16 +22,41 @@ class TV:
 
 # establish the methods for tv
     # turn on the tv (none)
+    def turnOn(self):
+        self.on_tv = True
     # turn off the tv (none)
+    def turnOff(self):
+        self.on_tv = False
     # returns the channel of the tv (int)
+    def getChannel(self):
+        return self.channel
     # sets a new channel for the tv (none)
     # there is 1  to 120 selection
+    def setChannel(self, channel):
+        if self.on_tv and 1 <= channel <= 120:
+            self.channel = channel
     # gets the volume level of the tv (int)
+    def getVolume(self):
+        return self.volume
     # sets a new volume level of the tv (none)
     # there is 1 to 7 range
+    def setVolume(self, volume):
+        if self.on_tv and 1 <= volume <= 7:
+            self.volume = volume
     # increase the channel number by 1 (none)
+    def channelUp(self):
+        if self.on_tv and self.channel < 120:
+            self.channel += 1
     # decrease the channel number by 2 (none)
+    def channelDown(self):
+        if self.on_tv and self.channel > 1:
+            self.channel -= 1
     # increase the volume level by 1 (none)
+    def volumeUp(self):
+        if self.on_tv and self.volume < 7:
+            self.volume += 1
     # decrease the volume level by 1 (none)
-
+    def volumeDown(self):
+        if self.on_tv and self.volume > 1:
+            self.volume -= 1
 # end
