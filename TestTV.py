86527b0d7aa698e58c6b3bd3027f13d2b1d7b1e6
Family <albon.136546120855@2depedqc.ph>
2023-05-19 18:35:44 +0800

    Defining the TestTV

--- TestTV.py
+++ TestTV.py
@@ -1,7 +1,10 @@
 '''continuation of the program to test the class'''
 
 # import the TV from the module created
+from class_TV import TV
+
 # construct the test driver
-# create two objects from class TV
+def TestTV():
+    # create two objects from class TV
 # recognize the command and print the output
 # run the program
