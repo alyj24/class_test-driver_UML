4efd7ace442c5b8f9aaeb457d36f44835b48a3ec
Family <albon.136546120855@2depedqc.ph>
2023-05-19 17:58:52 +0800

    Instruction

--- class_TV.py
+++ class_TV.py
@@ -2,3 +2,7 @@ print("Assignment 6".center(42, "="))
 print("\033[95m=" * 42)
 print("\033[94mCMPE-103-MODULE-4-INTRODUCTION-TO-OBJECT-ORIENTED-PROGRAMMING")
 print("\033[95m=" * 42)
+
+# create a python code that have a class named "TV" and a Test Driver program named
+# "TestTV" that will create two objects from class 'TV' and will produce the given outputs.
+
