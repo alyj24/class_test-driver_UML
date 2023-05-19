de804cb926cf9e6a76656327e86e73c53ecf3f7c
Family <albon.136546120855@2depedqc.ph>
2023-05-19 18:38:13 +0800

    Functions of lines added

--- TestTV.py
+++ TestTV.py
@@ -7,6 +7,10 @@ from class_TV import TV
 def TestTV():
     # create two objects from class TV
     # tv1 objects (channel is 30, volume is 3)
+    tv1 = TV(30, 3)
+    tv1.turnOn()
     # tv2 objects (channel is 3, volume is 2)
+    tv2 = TV(3, 2)
+    tv2.turnOn()
 # recognize the command and print the output
 # run the program
