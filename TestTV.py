48634378c6e60b2ffc7a57885e68951e15250bc9
Family <albon.136546120855@2depedqc.ph>
2023-05-19 18:34:19 +0800

    Pseudocode for Test Driver

--- TestTV.py
+++ TestTV.py
@@ -1,1 +1,7 @@
 '''continuation of the program to test the class'''
+
+# import the TV from the module created
+# construct the test driver
+# create two objects from class TV
+# recognize the command and print the output
+# run the program
