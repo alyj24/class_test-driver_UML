773fb7f893ceb745b1a29a9d3954fec8630db498
Family <albon.136546120855@2depedqc.ph>
2023-05-19 18:36:52 +0800

    Objects of tv1 and tv2 is defined

--- TestTV.py
+++ TestTV.py
@@ -6,5 +6,7 @@ from class_TV import TV
 # construct the test driver
 def TestTV():
     # create two objects from class TV
+    # tv1 objects (channel is 30, volume is 3)
+    # tv2 objects (channel is 3, volume is 2)
 # recognize the command and print the output
 # run the program
